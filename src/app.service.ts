f54faf2bc631d31706de33fb01bde05a4bbccbc5
techfaiyaz5 <faiyazkhanmoyal07860@gmail.com>
2026-04-08 13:20:32 +0530

    e-mail-problem-solved

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Program with jenkins!';
+    return 'Hello World Program with jenkins with Docker!';
   }
 }
